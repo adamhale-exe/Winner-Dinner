8ed844aa190c013e0594bc2c98f4456ce060cbbb
Adam Hale <adam.hale92@googlemail.com>
2023-11-25 18:45:06 +0000

    added basic styling and abstracted into components

--- winner-dinner/app/page.tsx
+++ winner-dinner/app/page.tsx
@@ -1,36 +1,13 @@
 import Image from "next/image";
+import RandomRecipe from "./RandomRecipe";
+import RecipeChooseForm from "./RecipeChooseForm";
 
 export default function Home() {
   return (
     <main className="text-black">
-      <div className="flex justify-center my-16">
-        <h1 className="text-5xl">Winner Dinner</h1>
-      </div>
       <div className="flex justify-center py-16">
-        <div className="flex flex-col text-center justify-center rounded-2xl bg-white border-black border-4 mx-10 w-80 h-80">
-          <h2>Choose a random recipe!</h2>
-        </div>
-        <div className="flex flex-col text-center justify-center rounded-2xl bg-white border-black border-4 mx-10 w-80 h-80">
-          <h2>I&apos;m hungry for:</h2>
-          <form className="flex flex-col items-center">
-            <label className="m-2">
-              <input
-                type="text"
-                className="rounded-xl border-black border-2"
-              ></input>
-            </label>
-            <label className="m2">
-              <input
-                type="text"
-                className="rounded-xl border-black border-2"
-              ></input>
-            </label>
-            <input
-              type="submit"
-              className="m-2 rounded-xl w-20 border-black border-2"
-            ></input>
-          </form>
-        </div>
+        <RandomRecipe />
+        <RecipeChooseForm />
       </div>
     </main>
   );
