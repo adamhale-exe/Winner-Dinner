0e31cdf5b4c955eb78c54133fe00357f7fed4f5d
Adam Hale <adam.hale92@googlemail.com>
2024-01-19 10:16:53 +0000

    added tag button and tag list

--- winner-dinner-app/src/components/RecipeChooseForm.jsx
+++ winner-dinner-app/src/components/RecipeChooseForm.jsx
@@ -1,26 +1,28 @@
+import TagButton from "./TagButton";
+
+const tagArr = [
+  "Quick", "Mexican", "Chips", "Chinese", "Pasta", "Cheesy", "Steak", "Broth"
+]
+
 export default function RecipeChooseForm() {
   return (
     <>
       <div className="flex flex-col text-center justify-center rounded-2xl bg-orange-400 border-black border-4 mx-10 w-80 h-80">
-        <h2>I&apos;m hungry for:</h2>
-        <form className="flex flex-col items-center">
-          <label className="m-2">
-            <input
-              type="text"
-              className="rounded-full border-black border-2"
-            ></input>
-          </label>
-          <label className="m2">
-            <input
-              type="text"
-              className="rounded-full border-black border-2"
-            ></input>
-          </label>
-          <input
-            type="submit"
-            className="m-2 rounded-full w-20 border-black border-2"
-          ></input>
-        </form>
+        <h2>I&apos;m hungry for...</h2>
+        {/* dynamically create a list of buttons.
+         these buttons are tags from the database that can be used to filter recipes on submit*/}
+          <ul>
+          {tagArr.map(i => <TagButton tag={i} />)}
+          </ul>
+        {/*
+         I need to fetch an array of popular options
+         iterate through the array with a .map to create the button element for each
+         button has state, pressed, not pressed.
+
+         List the pressed buttons/selected options with the ability to remove them
+
+         submit button to search for things matching those criteria
+           */}
       </div>
     </>
   );
---
+++ winner-dinner-app/src/components/TagButton.jsx
@@ -0,0 +1,7 @@
+export default function TagButton(tag) {
+  return (
+    <li>
+      <button>{tag}</button>
+    </li>
+  );
+}
