b2202b9db3cae8a99e3e4a0fdb626513aae70579
Adam Hale <adam.hale92@googlemail.com>
2024-01-31 13:24:49 +0000

    added get all request handling

--- winner-dinner-backend/controllers/recipetags.controller.mjs
+++ winner-dinner-backend/controllers/recipetags.controller.mjs
@@ -1,2 +1,8 @@
 //will need to import tags models from /models/tags.js
 import * as recipeTagsModel from "../models/recipeTagsModel.mjs";
+
+//first request to return all tags
+export async function getTags(req, res) {
+    const result = await tagsModel.getTags();
+    res.status(200).json({ success: true, payload: result });
+  }
