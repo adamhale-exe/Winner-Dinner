26b44b2e76e53c72e3dc728dcd3fa3004d6739ee
Adam Hale <adam.hale92@googlemail.com>
2024-01-31 13:37:51 +0000

    fixed missing curly brace

--- winner-dinner-backend/controllers/recipesTags.controller.mjs
+++ winner-dinner-backend/controllers/recipesTags.controller.mjs
@@ -66,15 +66,16 @@ export async function deleteRecipeTagByRecipeId(req, res) {
       error: "Invalid request 😞. Please review and try again!",
     });
   } else {
-  const recipeTagPair = req.body;
-  const result = await tagsModel.deleteRecipeTagByRecipeId(recipeTagPair);
-  if (result != null) {
-    res.status(200).json({ success: true, payload: result });
-  } else {
-    res.status(404).json({
-      success: false,
-      error:
-        "We couldn't find what you were looking for 😞. Does that recipe/tag exist?",
-    });
+    const recipeTagPair = req.body;
+    const result = await tagsModel.deleteRecipeTagByRecipeId(recipeTagPair);
+    if (result != null) {
+      res.status(200).json({ success: true, payload: result });
+    } else {
+      res.status(404).json({
+        success: false,
+        error:
+          "We couldn't find what you were looking for 😞. Does that recipe/tag exist?",
+      });
+    }
   }
 }
