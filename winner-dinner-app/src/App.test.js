88aec9f888334532547a9ce763676c0847c6e7de
adamhale-exe <adam.hale92@googlemail.com>
2024-02-29 16:15:13 +0000

    additional test for App completed

--- winner-dinner-app/src/App.test.js
+++ winner-dinner-app/src/App.test.js
@@ -1,6 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
+//I would like to mock the fetch requests that happen to speed up the test and also ensure I am only testing what is necessary
+
 jest.mock("./customHooks/getTags", () => ({
   __esModule: true,
   default: () => ["Tag1", "Tag2", "Tag3"],
@@ -16,4 +18,8 @@ test("renders Winner Dinner header", () => {
   expect(headerElement).toBeInTheDocument();
 });
 
-//I would like to mock the fetch requests that happen to speed up the test and also ensure I am only testing what is necessary
+test("renders random recipe button", () => {
+  render(<App />);
+  const randomButtonElement = screen.getByText("Choose a random recipe!");
+  expect(randomButtonElement).toBeInTheDocument();
+});
